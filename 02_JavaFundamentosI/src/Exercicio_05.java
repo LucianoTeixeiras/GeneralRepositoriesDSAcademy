e7df9059f9710c0d5eff8296e774153c79a3a302
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-04-01 23:15:32 -0300

    Commit Capitulo 2 - Operadores Unários

---
+++ 02_JavaFundamentosI/src/Exercicio_05.java
@@ -0,0 +1,13 @@
+public class Exercicio_05 {
+
+    // Operadores Unarios
+
+    public static void main(String[] args) {
+
+        int a = 0;
+        a = a + 10;
+
+        System.out.println(a);
+
+    }
+}
