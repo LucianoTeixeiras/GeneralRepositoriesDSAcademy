3aa976618b21c028153a73c9ac27edd25907c75f
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-05-02 16:59:03 -0300

    Commit de Ajuste de Modulos/Teste

--- 02_JavaFundamentos_I/src/Exercicio_00.java
+++ 02_JavaFundamentos_I/src/Exercicio_00.java
@@ -1,2 +1,11 @@
 public class Exercicio_00 {
+
+    //Organizacao do Codigo
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello DSA 00!!!");
+
+    }
 }
+
--- 02_JavaFundamentos_I/src/Exercicio_01.java
+++ 02_JavaFundamentos_I/src/Exercicio_01.java
@@ -4,7 +4,7 @@ public class Exercicio_01 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello DSA!!!");
+        System.out.println("Hello DSA 01!!!");
 
     }
 }
