3992e953533a71f0b10b4f88507881da259a5989
Luciano Teixeira <luciano.teixeiras@gmail.com>
2019-12-02 07:24:29 -0300

    Add files scripts
    Add files scripts

---
+++ FCD_BigDataAnalyticsRMAML/Cap02/01-script.R
@@ -0,0 +1,7 @@
+# Instalando PKG caret
+
+install.packages('caret', dependencies = TRUE)
+install.packages('randomForest', dependencies = TRUE)
+install.packages('ggplot2', dependencies = TRUE)
+install.packages('dplyr', dependencies = TRUE)
+install.packages('devtools', dependencies = TRUE)
