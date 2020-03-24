b0cf1a3d7afea96eabce9c8936477ded17b39bdc
Luciano Teixeira <luciano.teixeiras@gmail.com>
2020-03-24 07:52:27 -0300

    Cap 06 Machine Learning
    Cap 06 Machine Learning com Python e Spark

--- FCD_BigDataAnalyticsRMAML/Cap02/01-script.R
+++
@@ -1,7 +0,0 @@
-# Instalando PKG caret
-
-install.packages('caret', dependencies = TRUE)
-install.packages('randomForest', dependencies = TRUE)
-install.packages('ggplot2', dependencies = TRUE)
-install.packages('dplyr', dependencies = TRUE)
-install.packages('devtools', dependencies = TRUE)
